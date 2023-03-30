dc3dd8b364c25fec8058716e473e1db7e0bf51f3
MarkMa512 <42110231+MarkMa512@users.noreply.github.com>
2023-03-30 17:55:18 +0800

    created shell script for macOS

--- IS708_API/install_Linux.sh
+++ IS708_API/install_Linux.sh
@@ -1,3 +1,5 @@
+#! /bin/bash 
+
 sudo apt update
 sudo apt install ffmpeg
 pip3 install -r requirements.txt
---
+++ IS708_API/install_macOS.sh
@@ -0,0 +1,5 @@
+#! /bin/bash 
+
+brew install ffmpeg
+pip install -r requirements.txt
+brew install android-platform-tools
