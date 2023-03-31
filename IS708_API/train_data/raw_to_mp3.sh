c352f57dbc0abc6fcd7d348cb98285360f8f8a2d
MarkMa512 <42110231+MarkMa512@users.noreply.github.com>
2023-03-31 08:55:09 +0800

    recorganize for model training

--- IS708_API/train_data/raw_to_mp3.sh
+++ IS708_API/train_data/raw_to_mp3.sh
@@ -1,5 +1,7 @@
+# covert each raw file too mp3 file 
 for i in *.raw; do ffmpeg -f s16le -ar 44100 -ac 1 -i "$i" "${i%.*}.mp3"; done
 
 # for dir in .; do (cd "$dir" && mkdir raw_file && mv *.raw raw_file); done
 
+# move all raw file to the raw_file dir
 mkdir raw_file; mv *.raw raw_file;
