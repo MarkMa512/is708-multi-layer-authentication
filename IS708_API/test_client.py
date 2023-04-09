52bc0552b0bf049f9fe2d93ba59ffdda15fdb4ca
MarkMa512 <42110231+MarkMa512@users.noreply.github.com>
2023-04-09 23:39:18 +0800

    improve on documentation

--- IS708_API/test_client.py
+++ IS708_API/test_client.py
@@ -9,19 +9,19 @@ logging.basicConfig(
 ws = websocket.create_connection("ws://localhost:8086/")
 logging.info("Connected to server.")
 
-# read a raw audio file as a byte array
+# read a raw audio file
 with open('model_training/sample.raw', 'rb') as f:
     audio_file = f.read()
 
-# read a raw audio file as a byte array
+# read a gesture csv  file
 with open('model_training/Gesture/3/1677689358090.csv', 'rb') as f:
     gesture_file = f.read()
 
-# send the audio as a a byte array message to the server
+# send the audio as a binary message to the server
 ws.send_binary(audio_file)
 logging.info(f"Sent {len(audio_file)} bytes of audio data to the server.")
 
-# send the gesture as a a byte array message to the server
+# send the gesture asa binary message to the server
 ws.send_binary(gesture_file)
 logging.info(f"Sent {len(gesture_file)} bytes of gesture data to the server.")
 
@@ -31,6 +31,7 @@ while True:
         response = ws.recv()
         logging.info(f"Received response: {response}")
         if response == b'3':
+            # the expected response is b'3'
             logging.info("========== Server Setup Success! ==========")
     except websocket.WebSocketConnectionClosedException:
         logging.warning("WebSocket connection closed.")
