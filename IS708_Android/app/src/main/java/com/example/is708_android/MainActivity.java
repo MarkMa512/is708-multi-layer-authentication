5270374d84295a64e11ae2677d941db7cf32d0e4
MarkMa512 <ningzhi04@hotmail.com>
2023-04-16 14:42:05 +0800

    csv recording feature completed.

--- IS708_Android/app/src/main/java/com/example/is708_android/MainActivity.java
+++ IS708_Android/app/src/main/java/com/example/is708_android/MainActivity.java
@@ -2,8 +2,14 @@ package com.example.is708_android;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.os.Bundle;
 import android.os.Environment;
+import android.os.SystemClock;
 import android.util.Log;
 
 import android.media.MediaRecorder;
@@ -18,6 +24,7 @@ import org.java_websocket.handshake.ServerHandshake;
 import java.io.File;
 import java.io.FileInputStream;
 
+import java.io.FileWriter;
 import java.io.IOException;
 import java.net.URI;
 import java.net.URISyntaxException;
@@ -25,8 +32,10 @@ import java.net.URISyntaxException;
 import java.util.Timer;
 import java.util.TimerTask;
 
-public class MainActivity extends AppCompatActivity {
-    private Button recordButton;
+public class MainActivity extends AppCompatActivity implements SensorEventListener{
+
+    // Variables for recording audio
+    private Button recordAudioButton;
     private boolean isRecording = false;
     private Timer timer;
     private  WebSocketClient webSocketClient;
@@ -34,14 +43,23 @@ public class MainActivity extends AppCompatActivity {
 
     MediaRecorder mediaRecorder;
 
+    // Variables for recording gesture
+    private Button recordGestureButton;
+    private SensorManager sensorManager;
+    private Sensor accelerometer;
+    private Sensor gyroscope;
+    private boolean isRecordingGesture = false;
+    private File gestureFile;
+    private FileWriter fileWriter;
+    private long startTime;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        // Initialize the record button
-        recordButton = findViewById(R.id.capture_audio_button);
-        recordButton.setOnClickListener(new View.OnClickListener() {
+        // Initialize the record audio button
+        recordAudioButton = findViewById(R.id.capture_audio_button);
+        recordAudioButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 if (!isRecording) {
@@ -56,23 +74,47 @@ public class MainActivity extends AppCompatActivity {
         requestPermissions(new String[]{android.Manifest.permission.RECORD_AUDIO}, 1);
         // request for file write permission
         requestPermissions(new String[]{android.Manifest.permission.WRITE_EXTERNAL_STORAGE}, 1);
+
+        // Initialize the record gesture button
+        recordGestureButton = findViewById(R.id.capture_gesture_button);
+        recordGestureButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (!isRecordingGesture) {
+                    startRecordingGesture();
+                } else {
+                    stopRecordingGesture();
+                }
+            }
+        });
+
+        // Initialize SensorManager and Sensor
+        sensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+        accelerometer = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+        gyroscope = sensorManager.getDefaultSensor(Sensor.TYPE_GYROSCOPE);
+
+        // request for sensor permission
+        requestPermissions(new String[]{android.Manifest.permission.BODY_SENSORS}, 1);
+        //declare the normal permission HIGH_SAMPLING_RATE_SENSORS.
+        requestPermissions(new String[]{android.Manifest.permission.HIGH_SAMPLING_RATE_SENSORS}, 1);
     }
+
     private void startRecordingAudio(){
         runOnUiThread(new Runnable() {
             @Override
             public void run() {
-                recordButton.setText("Stop Recording");
+                recordAudioButton.setText("Stop Recording");
             }
         });
         isRecording = true;
 
-       timer = new Timer();
-       timer.schedule(new TimerTask() {
+        timer = new Timer();
+        timer.schedule(new TimerTask() {
            @Override
            public void run() {
                stopRecordingAudio();
            }
-       }, 3000);
+        }, 3000);
 
        // create the audio file
         try{
@@ -105,7 +147,7 @@ public class MainActivity extends AppCompatActivity {
         runOnUiThread(new Runnable() {
             @Override
             public void run() {
-                recordButton.setText("Record Audio");
+                recordAudioButton.setText("Record Audio");
                 Toast.makeText(MainActivity.this, "Recording Stopped", Toast.LENGTH_SHORT).show();
             }
         });
@@ -132,7 +174,7 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
-    private byte[] convertAudioToByteArray(File audioFile) throws IOException {
+    private byte[] convertFileToByteArray(File audioFile) throws IOException {
         // Create input stream from audio file
         FileInputStream inputStream = new FileInputStream(audioFile);
 
@@ -148,6 +190,94 @@ public class MainActivity extends AppCompatActivity {
         return byteArray;
     }
 
+    private void startRecordingGesture() {
+        isRecordingGesture = true;
+        runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                recordGestureButton.setText("Stop Gesture Recording");
+            }
+        });
+
+        timer = new Timer();
+        timer.schedule(new TimerTask() {
+            @Override
+            public void run() {
+                stopRecordingGesture();
+            }
+        }, 3000);
+
+        try {
+            // Create CSV file
+            gestureFile = new File(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS).getAbsolutePath(), "gesture.csv");
+            gestureFile.createNewFile();
+
+            // Initialize FileWriter
+            fileWriter = new FileWriter(gestureFile);
+
+            // Write CSV headers
+            fileWriter.write("Timestamp,AccelX,AccelY,AccelZ,GyroX,GyroY,GyroZ\n");
+
+            // Register sensor event listeners
+            sensorManager.registerListener(this, accelerometer, SensorManager.SENSOR_DELAY_FASTEST);
+            sensorManager.registerListener(this, gyroscope, SensorManager.SENSOR_DELAY_FASTEST);
+
+            // Record start time
+            startTime = SystemClock.elapsedRealtime();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void stopRecordingGesture() {
+        isRecordingGesture = false;
+        runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                recordGestureButton.setText("Record Gesture");
+                Toast.makeText(MainActivity.this, "Gesture data saved", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+
+        // Unregister sensor event listeners
+        sensorManager.unregisterListener(this, accelerometer);
+        sensorManager.unregisterListener(this, gyroscope);
+
+        try {
+            // Close FileWriter
+            fileWriter.close();
+        } catch (IOException e){
+            e.printStackTrace();
+        }
+        // cancel the timer
+        timer.cancel();
+    }
+
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+        if (isRecordingGesture) {
+            try {
+                // Calculate elapsed time
+                long elapsedTime = SystemClock.elapsedRealtime() - startTime;
+
+                // Write sensor data to CSV file
+                if (event.sensor.getType() == Sensor.TYPE_ACCELEROMETER) {
+                    fileWriter.write(elapsedTime + "," + event.values[0] + "," + event.values[1] + "," + event.values[2] + ",");
+                } else if (event.sensor.getType() == Sensor.TYPE_GYROSCOPE) {
+                    fileWriter.write(event.values[0] + "," + event.values[1] + "," + event.values[2] + "\n");
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+        // Do nothing
+    }
 
 
     public void sendDataOverWebSocket(byte[] byteArray, String serverUrl) throws IOException, URISyntaxException {
