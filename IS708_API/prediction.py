57948c7754c8e216966c3ad733808d251cc19555
MarkMa512 <42110231+MarkMa512@users.noreply.github.com>
2023-04-19 17:20:55 +0800

    Test on Intel Mac

--- IS708_API/prediction.py
+++ IS708_API/prediction.py
@@ -47,10 +47,11 @@ def predict_new_gesture(csv_file_path: str, clf: object) -> int:
     frequncy_dict = {i: np.count_nonzero(
         result_list == i) for i in range(1, 6)}
     logging.info(
-        "===== gesture prediction result (User:Count): " + str(frequncy_dict)+ " =====")
+        "===== gesture prediction result (User:Count): " + str(frequncy_dict) + " =====")
     counts = np.bincount(result_list)
     result = np.argmax(counts)
     return result
+
 
 def predict_new_gesture_proba(csv_file_path: str, clf: object) -> dict:
     """
@@ -77,13 +78,14 @@ def predict_new_gesture_proba(csv_file_path: str, clf: object) -> dict:
     total_reading_counts = sum(frequncy_dict.values())
     # probability_dict (dict): A dictionary of user and the probability of the user
     # calculated by deviding the number of times the user is predicted by the total number of rows in the CSV file / reading counts
-    probability_dict = {k: v / total_reading_counts for k, v in frequncy_dict.items()}
+    probability_dict = {k: v / total_reading_counts for k,
+                        v in frequncy_dict.items()}
     logging.info(
-        "----- gesture probability (User:Probability): " + str(probability_dict) +"-----")
+        "----- gesture probability (User:Probability): " + str(probability_dict) + "-----")
     return probability_dict
 
-def predict_new_gesture_integral(csv_file_path: str, clf: object) -> int:
 
+def predict_new_gesture_integral(csv_file_path: str, clf: object) -> int:
     """
     This function is retired as Row by Row model is used. 
     """
@@ -117,12 +119,12 @@ def predict_new_gesture_integral(csv_file_path: str, clf: object) -> int:
 def predict_new_audio(new_audio_path: str, svm: object, n_mfcc=20) -> int:
     """
     Predict the source of new audio at new_audio_path based on the model svm trained. 
-    
+
     Parameters:
         - new_audio_path (str): Path to the folder containing new audio to be predicted. 
         - svm: trained SVM model 
         - n_mfcc (int): Number of Mel-frequency cepstral coefficients (MFCC) to extract, default = 20. 
-    
+
     Returns:
         - result_list[0](int): The predicted userID for the given new audio. 
     """
@@ -134,30 +136,31 @@ def predict_new_audio(new_audio_path: str, svm: object, n_mfcc=20) -> int:
 
     # Padding / truncating the input list to ensure that they are consistent to the length of the training data
     """ By default: padding using tensor flow"""
-    # from tensorflow.keras.preprocessing.sequence import pad_sequences
-    # input_list = pad_sequences(
-    #     input_list, maxlen=2500, dtype='float32', padding='post', truncating='post')
+    from tensorflow.keras.preprocessing.sequence import pad_sequences
+    input_list = pad_sequences(
+        input_list, maxlen=2500, dtype='float32', padding='post', truncating='post')
 
     """For Apple Silicon Mac: padding using numpy for test with apple silicon"""
-    if input_list[0].__len__() < 2500:
-        input_list[0].extend([0] * (2500 - input_list[0].__len__())) # padding with 0
-    else:
-        input_list[0] = input_list[0][:2500]
+    # if input_list[0].__len__() < 2500:
+    #     input_list[0].extend([0] * (2500 - input_list[0].__len__())) # padding with 0
+    # else:
+    #     input_list[0] = input_list[0][:2500]
 
     result_list = svm.predict(input_list)
     logging.info("===== audio prediction result (User): " +
                  str(result_list[0]) + " =====")
     return result_list[0]
 
+
 def predict_new_audio_proba(new_audio_path: str, svm: object, n_mfcc=20) -> dict:
     """
     Produce the probability of each user for the new audio at new_audio_path based on the model svm trained.
-    
+
     Parameters:
         - new_audio_path (str): Path to the folder containing new audio to be predicted. 
         - svm: trained SVM model 
         - n_mfcc (int): Number of Mel-frequency cepstral coefficients (MFCC) to extract, default = 20. 
-    
+
     Returns:
         - probabilities (dict):  {userID:probability}
 
@@ -170,26 +173,29 @@ def predict_new_audio_proba(new_audio_path: str, svm: object, n_mfcc=20) -> dict
 
     # Padding / truncating the input list to ensure that they are consistent to the length of the training data
     """ By default: padding using tensor flow"""
-    # from tensorflow.keras.preprocessing.sequence import pad_sequences
-    # input_list = pad_sequences(
-    #     input_list, maxlen=2500, dtype='float32', padding='post', truncating='post')
+    from tensorflow.keras.preprocessing.sequence import pad_sequences
+    input_list = pad_sequences(
+        input_list, maxlen=2500, dtype='float32', padding='post', truncating='post')
 
     """For Apple Silicon Mac: padding using numpy for test with apple silicon"""
-    if input_list[0].__len__() < 2500:
-        input_list[0].extend([0] * (2500 - input_list[0].__len__())) # padding with 0
-    else:
-        input_list[0] = input_list[0][:2500]
+    # if input_list[0].__len__() < 2500:
+    #     input_list[0].extend([0] * (2500 - input_list[0].__len__())) # padding with 0
+    # else:
+    #     input_list[0] = input_list[0][:2500]
 
     result_list = svm.predict_proba(input_list)
-    probability_dict = {k: v for k, v in enumerate(result_list[0],1)}
+    probability_dict = {k: v for k, v in enumerate(result_list[0], 1)}
     logging.info(
-        "----- audio probability (User:Probability): " + str(probability_dict) +"-----")
+        "----- audio probability (User:Probability): " + str(probability_dict) + "-----")
     return probability_dict
+
 
 """
 Fusion Logic
 """
-def combine_predict(audio_result: int, gesture_result: int, new_audio_path:str,audio_model:object, csv_file_path:str, gesture_model:object) -> int:
+
+
+def combine_predict(audio_result: int, gesture_result: int, new_audio_path: str, audio_model: object, csv_file_path: str, gesture_model: object) -> int:
     """
     Combine the prediction result from audio and gesture model and return the final prediction result.
 
@@ -200,7 +206,7 @@ def combine_predict(audio_result: int, gesture_result: int, new_audio_path:str,a
         - audio_model: trained SVM model for audio
         - csv_file_path (str): Path to the folder containing new gesture to be predicted.
         - gesture_model: trained SVM model for gesture
-    
+
     Returns:
         - If both models predict the same user, simply return the user.
         - max(joint_prob, key=joint_prob.get)(int): The predicted userID for the given new audio and gesture, with the highest joint probability.
@@ -218,12 +224,13 @@ def combine_predict(audio_result: int, gesture_result: int, new_audio_path:str,a
         joint_prob = {}
         # for each user, calculate the joint probability
         for user in range(1, 6):
-            # since the action of speaking does not affect the gesture performance, 
+            # since the action of speaking does not affect the gesture performance,
             # and both models are trained independently, we assume that the joint probability is the product of the individual probabilities
             joint_prob[user] = audio_prob[user] * gesture_prob[user]
-        logging.info("----- joint probability (User:Probability): " + str(joint_prob) +"-----")
+        logging.info("----- joint probability (User:Probability): " +
+                     str(joint_prob) + "-----")
         return max(joint_prob, key=joint_prob.get)
-        
+
 
 """
 Testing
@@ -244,7 +251,8 @@ if __name__ == "__main__":
     gesture_result = predict_new_gesture(csv_file_path, gesture_model)
 
     # combine the prediction results
-    combine_prediction = combine_predict(audio_result, gesture_result, new_audio_path, audio_model, csv_file_path, gesture_model)
+    combine_prediction = combine_predict(
+        audio_result, gesture_result, new_audio_path, audio_model, csv_file_path, gesture_model)
 
     print(f'Audio Result: {audio_result}')
     print(f'Gesture Result: {gesture_result}')
